fc98bdad1b4220868969708803fdf50bcf6cf525
shanermua <shanermua@gmail.com>
2021-11-08 18:38:05 +0800

    initial commit

---
+++ app/src/main/java/com/shaner/base/data/LoginDataSource.kt
@@ -0,0 +1,54 @@
+package com.shaner.base.data
+
+import com.shaner.base.data.model.LoggedInUser
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
+
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    private val client = OkHttpClient()
+    private val json: MediaType = "application/json".toMediaType()
+
+//    val repository = LoginRepository{};
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val baseUrl = "http://10.2.2.144:8088"
+            val url = "$baseUrl/"
+            var responseBody = ""
+
+            val requestBodyJson = JSONObject()
+            requestBodyJson.put("username", username)
+            requestBodyJson.put("password", password)
+            val requestBody: RequestBody = requestBodyJson.toString().toRequestBody(this.json)
+            val builder = Request.Builder()
+            builder.url(url)
+            builder.addHeader("Content-Type","application/json")
+                .addHeader("Version","@string/app_version")
+                .post(requestBody)
+            client.newCall(builder.build()).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    throw e
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    responseBody = response.body!!.string()
+                }
+            })
+            val result = LoggedInUser(java.util.UUID.randomUUID().toString(), responseBody)
+            return Result.Success(result)
+        }catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
